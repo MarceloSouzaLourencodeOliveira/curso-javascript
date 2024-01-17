a0ec998d239a6fdf2c0141f1aea122c2e6c0652a
Marcelo Oliveira <marcelosouzalourencooliveira@hotmail.com>
2024-01-17 19:21:07 -0300

    atualização

--- exercicicos/aula15/index.js
+++ exercicicos/aula15/index.js
@@ -4,13 +4,13 @@ let num1 = 9;
 
 //let num2 = Math.floor(num1); ARREDONDA PARA BAIXO
 //let num2 = Math.ceil(num1); ARREDONDA PARA CIMA
-//let num2 = Math.round(num1); AREDONDA CONFORME O NUMERO FRUTUANTE. EXEM: 9.49 ARREDONDA PARA BAIXO. 9.50 ARREDONDA PARA CIMA
+//let num2 = Math.round(num1); AREDONDA DE FORMA AUTOMATICA
 //console.log(num2);
 
 
-console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-//console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
-//console.log(aleatorio);
-//console.log(Math.pow(2, 10));
-//console.log(2 ** 10);
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //PEGA O MAIOR NUMERO DE UMA LISTA DE NUMEROS
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //PEGA O MENOR NUMERO DE UMA LISTA DE NUMEROS
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio); // "RANDOM" PARA GERAR NUMEROS ALEATORIOS ENTRE 0 E 1, POREM O 1 NUNCA É INCLUIDO
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
