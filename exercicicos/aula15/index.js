6076c5e5d83ff1496b3a09a0d1c90d18d1e096e7
Marcelo Oliveira <marcelosouzalourencooliveira@hotmail.com>
2024-01-17 19:15:35 -0300

    atualização

--- exercicicos/aula15/index.js
+++ exercicicos/aula15/index.js
@@ -1,11 +1,16 @@
-let num1 = 9.15;
+let num1 = 9;
+//console.log(num1 ** 0.5);
+//console.log(100 / 0);
+
 //let num2 = Math.floor(num1); ARREDONDA PARA BAIXO
 //let num2 = Math.ceil(num1); ARREDONDA PARA CIMA
 //let num2 = Math.round(num1); AREDONDA CONFORME O NUMERO FRUTUANTE. EXEM: 9.49 ARREDONDA PARA BAIXO. 9.50 ARREDONDA PARA CIMA
 //console.log(num2);
 
 
-//console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
 //console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
-const aleatorio = Math.random();
-console.log(aleatorio);
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+//console.log(aleatorio);
+//console.log(Math.pow(2, 10));
+//console.log(2 ** 10);
