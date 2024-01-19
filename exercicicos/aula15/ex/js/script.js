840887bbcd23cf58ed685eab23c7cec0ca7ef49f
Marcelo Oliveira <marcelosouzalourencooliveira@hotmail.com>
2024-01-18 23:00:51 -0300

    atualização

--- exercicicos/aula15/ex/js/script.js
+++ exercicicos/aula15/ex/js/script.js
@@ -1,8 +1,23 @@
-let numero = prompt('Digite um número');
+/*let numero = prompt('Digite um número');
 numero
 const numeroTitulo = document.getElementById('numero-titulo');
 
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>Seu número + 2 é ${numero + 2}.</p>`;
+texto.innerHTML = `<p>Seu número + 2 é ${numero + 2}.</p>`;*/
+
+
+const numero = Number(prompt('Digite um número:'));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero;
+
+texto.innerHTML = '';
+texto.innerHTML += `<p>raiz quadrada: ${numero ** 0.5}.<p/>`;
+texto.innerHTML += `<p>É Nan: ${Number.isNaN(numero)}.<p/>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}.</p>`;
+texto.innerHTML += `<p>arredonda para baixo ${Math.floor(numero)}.</p>`;
+texto.innerHTML += `<p>arredonda para cima ${Math.ceil(numero)}.</p>`;
+texto.innerHTML += `<p>com duas casas decimais ${numero.toFixed(2)}.</p>`;
