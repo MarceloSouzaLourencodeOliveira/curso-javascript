5da0bb4ff23dbfe0b61750f5a80edf1d23b451ad
Marcelo Oliveira <marcelosouzalourencooliveira@hotmail.com>
2024-01-19 18:52:36 -0300

    criei exercício 16

---
+++ exercicicos/aula16/index.js
@@ -0,0 +1,31 @@
+//            012345678910 arrais são indexados 
+const nome = 'luiz otavio'
+//console.log(nome[10]);
+
+//                0        1        2
+const alunos = ['luiz', 'maria', 'joão'];
+
+//const removido = alunos.pop(); // remove index do final
+const removido = alunos.shift(); // remove do começo
+console.log(removido);
+console.log(alunos);
+
+alunos.unshift('luiza'); //adiciona no começo
+alunos.unshift('fabio');
+
+
+//alunos.push('karina'); //adiciona no fim 
+//alunos.push('markito');
+
+ 
+//alunos[alunos.length] = 'luiza'; //adiciona no fim 
+//alunos[alunos.length] = 'fabio';
+//alunos[alunos.length] = 'luana';
+
+//alunos[0] = 'eduardo'; // altera
+//alunos[3] = 'luiza'; // adicionando
+
+
+//console.log(alunos);
+//console.log(alunos[0]); // acessando o index
+//console.log(alunos[2]);
