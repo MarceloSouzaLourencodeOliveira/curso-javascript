8bf27a6a5706ed46f11f04fc287d9d8dcd3106a7
Marcelo Oliveira <marcelosouzalourencooliveira@hotmail.com>
2024-01-19 19:06:17 -0300

    atualização

--- exercicicos/aula16/index.js
+++ exercicicos/aula16/index.js
@@ -3,15 +3,27 @@ const nome = 'luiz otavio'
 //console.log(nome[10]);
 
 //                0        1        2
-const alunos = ['luiz', 'maria', 'joão'];
+const alunos = ['luiz', 'maria', 'joão']; // luiza, eduardo
+
+console.log(typeof alunos);
+
+
+//alunos.push('luiza');
+//alunos.push('eduardo');
+//console.log(alunos.slice(0, -1)); // fatia o array a partir do index selecionando ate o index que vai  delimitar a fatia. ex: do index 0, ao -1 (relativo ao final do array. menos um numero do final)
+
+//console.log(alunos[50]);
+
+//delete alunos[1]; // deleta index selecionado 
+//console.log(alunos);
 
 //const removido = alunos.pop(); // remove index do final
-const removido = alunos.shift(); // remove do começo
-console.log(removido);
-console.log(alunos);
+//const removido = alunos.shift(); // remove index do começo
+//console.log(removido);
+//console.log(alunos);
 
-alunos.unshift('luiza'); //adiciona no começo
-alunos.unshift('fabio');
+//alunos.unshift('luiza'); //adiciona no começo
+//alunos.unshift('fabio');
 
 
 //alunos.push('karina'); //adiciona no fim 
